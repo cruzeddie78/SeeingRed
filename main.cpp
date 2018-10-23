21b5109e97a28929b10ec5dcdb6ffd31c04e269b
cruzeddie78 <ecruz19@mail.csuchico.edu>
2018-10-22 17:51:51 -0700

    took out the color of green and blue out of the pictures

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  
+
   cout << argc << " arguments!" << endl;
   cout << argv[1] << endl;
 
@@ -16,25 +16,38 @@ int main(int argc, char* argv[])
 
 
   if (argc!=2)
-{
-  cout<<"Please specify one image file!\n";
+  {
+    cout<<"Please specify one image file!\n";
 
-}
+  }
 
 
   string filename=argv[1];
   Bitmap image;
+  vector<vector<Pixel> > imagePixels;
 
   image.open(filename);
 
   if(image.isImage())
-{  
-  vector<vector< Pixel> > imagePixels = image.toPixelMatrix();
-  cout<<" kendall.bmp is "<< imagePixels.size()<<"wide and "<<imagePixels[0].size()  << "high."<<endl;
-}
+  {  
+    imagePixels = image.toPixelMatrix();
+    cout<<" kendall.bmp is "<< imagePixels.size()<<"wide and "<<imagePixels[0].size()  << "high."<<endl;
+  }
   else
-{ 
-  cout<< "Image file must be a bitmap with 24-bit color depth.\n";
-}
-return 0;
+  { 
+    cout<< "Image file must be a bitmap with 24-bit color depth.\n";
+  }
+    for(int col=0;col<imagePixels.size();col++)
+    { 
+      for(int row=0;row<imagePixels[col].size();row++)
+      {
+        Pixel color=imagePixels[col][row];
+        color.green=0;
+        color.blue=0;
+        imagePixels[col][row]=color;
+      }
+    }
+  image.fromPixelMatrix(imagePixels);
+  image.save("redness.bmp");
+  return 0;
 }
